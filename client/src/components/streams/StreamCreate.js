1d78cf68d4b9f4a22581c430432edbc9a50ea273
hing-sf <hing.tang.sf@gmail.com>
2018-12-13 21:21:59 -0800

    added axio, redux thunk, able to post form to db.json

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends Component {
 	renderError({ error, touched }) {
@@ -25,9 +27,11 @@ class StreamCreate extends Component {
 		);
 	};
 
-	onSubmit(formValues) {
+	onSubmit = formValues => {
 		// console.log(formValues);
-	}
+
+		this.props.createStream(formValues);
+	};
 
 	render() {
 		// console.log(this.props);
@@ -54,7 +58,12 @@ const validate = formValues => {
 	return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
 	form: 'streamCreate',
 	validate: validate
 })(StreamCreate);
+
+export default connect(
+	null,
+	{ createStream }
+)(formWrapped);
